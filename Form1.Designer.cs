f09fef47d21b7d27690af1428fa17fe323290782
Weilderman <41922486+mayezrahman@users.noreply.github.com>
2023-02-24 03:13:26 +0000

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿
+namespace magic_8_bot1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.tmrChangeImage = new System.Windows.Forms.Timer(this.components);
+            this.imageListBotTalk = new System.Windows.Forms.ImageList(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Consolas", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Image = global::magic_8_bot1.Properties.Resources.windows_98;
+            this.button1.Location = new System.Drawing.Point(125, 320);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(200, 60);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "ASK";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.DodgerBlue;
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox1.Location = new System.Drawing.Point(12, 5);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(448, 109);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "HEY THERE BRO! I AM THE MAGIC 8 BOT, AND MY CURRENT JOB IS TO TELL PEOPLE\'S FORTU" +
+    "NES AND ANSWER YES/NO QUESTIONS. ASK ME ANY YES/NO QUESTION, FOR I AM A FRIENDLY" +
+    " ROBOT!";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::magic_8_bot1.Properties.Resources._8bot1;
+            this.pictureBox1.Location = new System.Drawing.Point(90, 120);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(269, 194);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.textBox2.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox2.Location = new System.Drawing.Point(135, 386);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(175, 22);
+            this.textBox2.TabIndex = 3;
+            // 
+            // tmrChangeImage
+            // 
+            this.tmrChangeImage.Enabled = true;
+            this.tmrChangeImage.Interval = 300;
+            // 
+            // imageListBotTalk
+            // 
+            this.imageListBotTalk.ColorDepth = System.Windows.Forms.ColorDepth.Depth24Bit;
+            this.imageListBotTalk.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageListBotTalk.ImageStream")));
+            this.imageListBotTalk.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageListBotTalk.Images.SetKeyName(0, "8bot1.png");
+            this.imageListBotTalk.Images.SetKeyName(1, "8bot2.png");
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(472, 420);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Magic 8 Robot";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Timer tmrChangeImage;
+        private System.Windows.Forms.ImageList imageListBotTalk;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+using System.Threading;
+
+namespace magic_8_bot1
+{
+    //This is the magic 8 bot application, and it contains a robot who has a job to answer your questions.
+    //Created by Weilderman on 2/21/2023, in Tennesse, United States.
+    //The program requires .net to be installed on your computer.
+    //The magic 8 bot loves soda, basketball, problem solving, and shooting evil robots!
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int rannum = 0;
+            Random r = new Random();
+            rannum = r.Next(1, 6);
+            switch (rannum)
+            {
+                case 1:
+                    textBox2.Text = "It is certain.";
+                    SoundPlayer sound = new SoundPlayer(Properties.Resources._8bot_line2);
+                    sound.Play();
+                    break;
+                case 2:
+                    textBox2.Text = "As I see it, yes.";
+                    SoundPlayer sound2 = new SoundPlayer(Properties.Resources._8bot_line1);
+                    sound2.Play();
+                    break;
+                case 3:
+                    textBox2.Text = "Reply hazy, try again.";
+                    SoundPlayer sound3 = new SoundPlayer(Properties.Resources._8bot_line3);
+                    sound3.Play();
+                    break;
+                case 4:
+                    textBox2.Text = "Probably not.";
+                    SoundPlayer sound4 = new SoundPlayer(Properties.Resources._8bot_line4);
+                    sound4.Play();
+                    break;
+                case 5:
+                    textBox2.Text = "Definetely no.";
+                    SoundPlayer sound5 = new SoundPlayer(Properties.Resources._8bot_line5);
+                    sound5.Play();
+                    break;
+                default:
+                    textBox2.Text = "Don't count on it.";
+                    SoundPlayer sound6 = new SoundPlayer(Properties.Resources._8bot_line4);
+                    sound6.Play();
+                    break;
+            }
+        }
+    }
+}
